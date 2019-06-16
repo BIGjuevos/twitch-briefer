b4852f721fac397d5229275f66b2acdfb76457d4
Ryan Null <ryan.null@gmail.com>
2019-06-15 21:15:33 -0700

    wow - it was this

--- src/helpers/db.py
+++ src/helpers/db.py
@@ -17,17 +17,17 @@ def set_data(thing, value):
 def get_data(thing):
     with connection.cursor() as cursor:
         if thing == "map":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('lat',))
-            lat = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("lat",))
+            lat = cursor.fetchone()["val"]
 
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('lng',))
-            lng = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("lng",))
+            lng = cursor.fetchone()["val"]
 
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('hdg',))
-            hdg = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("hdg",))
+            hdg = cursor.fetchone()["val"]
 
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('gs',))
-            gs = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("gs",))
+            gs = cursor.fetchone()["val"]
 
             return {
                 'lat': lat,
@@ -36,25 +36,25 @@ def get_data(thing):
                 'gs': gs
             }
         elif thing == "hdg":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('hdg',))
-            return cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("hdg",))
+            return cursor.fetchone()["val"]
         elif thing == "gs":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('gs',))
-            return cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("gs",))
+            return cursor.fetchone()["val"]
         elif thing == "tas":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('tas',))
-            return cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("tas",))
+            return cursor.fetchone()["val"]
         elif thing == "alt":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('alt',))
-            return cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("alt",))
+            return cursor.fetchone()["val"]
         elif thing == "rte":
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('dep',))
-            dep = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("dep",))
+            dep = cursor.fetchone()["val"]
 
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('arr',))
-            arr = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("arr",))
+            arr = cursor.fetchone()["val"]
 
-            cursor.execute("SELECT * from databits WHERE nam=%s", ('rte',))
-            rte = cursor.fetchone()['val']
+            cursor.execute("SELECT * from databits WHERE nam=%s", ("rte",))
+            rte = cursor.fetchone()["val"]
 
             return dep + " " + rte + " " + arr
