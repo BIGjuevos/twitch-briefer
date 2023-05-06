acf0bb50a4517a96adb3a6ce9285bfd59567ba1e
Ryan Null <ryan.null@gmail.com>
2023-05-05 21:45:35 -0700

    dynamic hosts for redirect

--- app/app/main.py
+++ app/app/main.py
@@ -40,7 +40,8 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           host=request.base_url,)
 
 
 @app.route('/status')
